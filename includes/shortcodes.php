6d9d7a75b47c56a5a875725748d0d1eca0380e59
davidkim10 <davidykim10@gmail.com>
2023-02-16 14:19:04 -0500

    refactor code

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -5,51 +5,35 @@ $webinar_shortcode_key = $mcp_cf7::KEY_WEBINARS;
 add_shortcode($workshop_shortcode_key, 'create_workshop_shortcode');
 add_shortcode($webinar_shortcode_key, 'create_webinar_shortcode');
 
-function create_workshop_shortcode() {
-    global $mcp_cf7;
-    $option_key = $mcp_cf7::DB_KEY_WORKSHOPS;
-    $defaultOption = '<select><option>-- No events at this time --</option>';
-    $options = get_option($option_key);
-    $output = '';
-    
-    if(!empty($options)){
-        $output = '<select class="mcp_workshops_field">';
-        foreach($options as $option){
-            $id = $option['id'];
-            $location = $option['location'];
-            $date = $option['date'];
-            $time = date("g:iA", strtotime($option['time']));
-            $output .= '<option value="'.$id.'">'.$location.' - '.$date.' at '.$time.'</option>';
-        }
-        $output .= '</select>';
-    } else {
-       $output = $defaultOption;
-    }
+function create_select_field($options, $option_key, $id_key, $defaultOptionClass) {
+    $defaultOption = "<select class=\"$defaultOptionClass\"><option>-- No events at this time --</option></select>";
+    $output = "<select class=\"$option_key\">";
 
+    if(empty($options)) {
+        return $defaultOption;
+    }
+    
+    foreach($options as $option){
+        $id = $option[$id_key];
+        $location = $option['location'];
+        $date = $option['date'];
+        $time = date("g:iA", strtotime($option['time']));
+        $output .= "<option value=\"$id\">$location - $date at $time</option>";
+    }
+    $output .= "</select>";
     return $output;
 }
 
+function create_workshop_shortcode() {
+    global $mcp_cf7;
+    $option_key = 'mcp_workshops_field';
+    $options = get_option($mcp_cf7::DB_KEY_WORKSHOPS);
+    return create_select_field($options, $option_key, 'id', $option_key . ' mcp-empty');
+}
 
 function create_webinar_shortcode() {
     global $mcp_cf7;
-    $option_key = $mcp_cf7::DB_KEY_WEBINARS;
-    $defaultOption = '<select><option>-- No events at this time --</option>';
-    $options = get_option($option_key);
-    $output = '';
-    
-    if(!empty($options)){
-        $output = '<select class="mcp_webinars_field">';
-        foreach($options as $option){
-            $eventId = $option['eventId'];
-            $location = $option['location'];
-            $date = $option['date'];
-            $time = date("g:iA", strtotime($option['time']));
-            $output .= '<option value="'.$eventId.'">'.$location.' - '.$date.' at '.$time.'</option>';
-        }
-        $output .= '</select>';
-    } else {
-       $output = $defaultOption;
-    }
-
-    return $output;
+    $option_key = 'mcp_webinars_field';
+    $options = get_option($mcp_cf7::DB_KEY_WEBINARS);
+    return create_select_field($options, $option_key, 'eventId', $option_key . ' mcp-empty');
 }
